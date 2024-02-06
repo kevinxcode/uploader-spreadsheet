61ced51cbb30a51078febd70dea6b1ffdce14f7b
kevin alnizar <96331929+kevinxcode@users.noreply.github.com>
2024-02-06 18:39:21 +0700

    updt

--- application/models/Mexport.php
+++ application/models/Mexport.php
@@ -14,67 +14,6 @@ class Mexport extends CI_Model{
 
   
 
-	public function generate($gid){
-        $this->sitedb = $this->load->database('sitedb', TRUE);
-		$sql = "select _gid,_nik,_name,_dept,_jab,_company_label, _company_name from workplaze.employee_profile('$gid') limit 1";
-		$query = $this->sitedb->query($sql);
-		$list = $query->result();
-        $count = count($list);
-        if($count>0){
-            foreach($list as $dt){
-                $_gid = $dt->_gid;
-                $_nik = $dt->_nik;
-                $_name = $dt->_name;
-                $_dept = $dt->_dept;
-                $_jab = $dt->_jab;
-                $_company_label = $dt->_company_label;
-            }
-            $n_exp = explode(" ", $_name);
-            // $final_name = strtolower($n_exp[0].'.'.$n_exp[1]); 
-            $final_name = strtolower(str_replace(" ",".", $_name)); 
-            if($_company_label=='SG'){
-                $img = 'logo-02.png';
-            }else{
-                $img = 'logo-01.png';
-            }
-            $date = date('M Y', strtotime(date('Y-m-d H:i:s')));
-            $new_ref = date('Y').'-'.$_gid;
-            // (B) CREATE QR CODE
-            $code_n = base64_encode(base64_encode($new_ref).'_'.$date);
-            $qr = QrCode::create($final_name."_".$code_n)
-            // (B1) CORRECTION LEVEL
-            ->setErrorCorrectionLevel(new ErrorCorrectionLevelHigh())
-            // (B2) SIZE & MARGIN
-            ->setSize(350)
-            ->setMargin(5)
-            // (B3) COLORS
-            ->setForegroundColor(new Color(0, 0, 0))
-            ->setBackgroundColor(new Color(255, 255, 255));
-            // (B4) ATTACH LOGO
-            $logo = Logo::create("assets/qr-logo/".$img."")
-            ->setResizeToWidth(75);
-            // (C) OUTPUT QR CODE
-            $writer = new PngWriter();
-            $result = $writer->write($qr, $logo);
-            // $result = $writer->write($qr);
-           
-            $qr_fix =  $result->getDataUri();
-            $arr = array(
-                "msg" => 'succcess',
-                "data" => $list,
-                "qr" => $qr_fix, 
-            );
-            return $arr;
-        }else{
-            $arr = array(
-                "msg" => 'error',
-                "data" => '',
-                "qr" => '', 
-            );
-            return $arr;
-        }
-    }
-
     function spreadsheet($myObj,$file){
         // Create a new spreadsheet object
         $spreadsheet = new Spreadsheet();
@@ -140,9 +79,7 @@ class Mexport extends CI_Model{
         $writer->save('php://output');
     }
 
-    function is_base64($s){
-        return !(base64_decode($s, true) === false);
-    }
+    
 
 
 
